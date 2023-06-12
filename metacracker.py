0a211fdd230dab88b92867bcb58a1d0d1c7a06f8
L3ster in 1337 <44538226+L3ster1337@users.noreply.github.com>
2023-06-12 12:02:36 -0300

    Update metacracker.py

--- metacracker.py
+++ metacracker.py
@@ -1,33 +1,26 @@
+from PIL import Image
 import os
 import sys
-import PyPDF2
 import curses
 
-def extract_pdf_metadata(file_path):
-    with open(file_path, 'rb') as file:
-        reader = PyPDF2.PdfReader(file)
-        metadata = reader.metadata
+
+def extract_file_metadata(file_path):
+    with Image.open(file_path) as img:
+        metadata = img.info
     return {key: metadata[key] for key in metadata}
 
-def save_pdf_metadata(file_path, metadata):
-    with open(file_path, 'rb') as file:
-        reader = PyPDF2.PdfReader(file)
-        writer = PyPDF2.PdfWriter()
 
-        for page in reader.pages:
-            writer.add_page(page)
+def save_file_metadata(file_path, metadata):
+    with Image.open(file_path) as img:
+        img.save(file_path, **metadata)
 
-        writer.add_metadata(metadata)
-        
-        with open(file_path, 'wb') as output_file:
-            writer.write(output_file)
 
 def main(stdscr, file_path):
     if not os.path.exists(file_path):
         print(f"File not found: {file_path}")
         sys.exit(1)
 
-    metadata = extract_pdf_metadata(file_path)
+    metadata = extract_file_metadata(file_path)
     quitting_menu = False
     quit_key = ""
     editing = False
@@ -42,10 +35,16 @@ def main(stdscr, file_path):
 
         # Display metadata
         keys = list(metadata.keys())
-        for idx, key in enumerate(keys):
-            value = metadata[key]
-            display_text = f"{key}: {value}"
-            stdscr.addstr(idx, 0, display_text)
+        if not keys:
+            stdscr.addstr(0, 0, "No metadata available")
+        else:
+            for idx, key in enumerate(keys):
+                value = metadata[key]
+                if isinstance(value, str):
+                    display_text = f"{key}: {value}"
+                else:
+                    display_text = f"{key}: {str(value)}"
+                stdscr.addstr(idx, 0, display_text)
 
         if quitting_menu:
             # Display "Quit menu" in the footer
@@ -55,7 +54,7 @@ def main(stdscr, file_path):
             stdscr.addstr(height - 1, 0, "Editing", curses.A_REVERSE)
 
         # Draw the cursor
-        if editing:
+        if editing and keys:
             stdscr.addstr(cursor_line, cursor_position, " ", curses.A_REVERSE)
 
         stdscr.refresh()
@@ -77,50 +76,71 @@ def main(stdscr, file_path):
                     quit_key = ""
             elif ch < 256:
                 quit_key += chr(ch)
-        
+
         # Editing mode
-        elif ch == 14:  # Ctrl + n
+        elif ch == 14:  # Ctrl + N
             editing = not editing
-            if editing:
-                cursor_position = len(keys[0]) + 2 if keys else 0
+            if editing and keys:
+                cursor_position = len(keys[0]) + 2
             else:
                 cursor_position = 0
 
-        elif editing:
+        elif editing and keys:
             key = keys[cursor_line]
             value = metadata[key]
-            max_cursor_position = len(key) + 2 + len(value)
+            if isinstance(value, str):
+                max_cursor_position = len(key) + 2 + len(value)
+            else:
+                max_cursor_position = len(key) + 2 + len(str(value))
 
             if ch == curses.KEY_DOWN and cursor_line < len(keys) - 1:
                 cursor_line += 1
-                cursor_position = min(cursor_position, len(keys[cursor_line]) + 2 + len(metadata[keys[cursor_line]]))
+                value = metadata[keys[cursor_line]]
+                if isinstance(value, str):
+                    cursor_position = min(cursor_position, len(keys[cursor_line]) + 2 + len(value))
+                else:
+                    cursor_position = min(cursor_position, len(keys[cursor_line]) + 2 + len(str(value)))
             elif ch == curses.KEY_UP and cursor_line > 0:
                 cursor_line -= 1
-                cursor_position = min(cursor_position, len(keys[cursor_line]) + 2 + len(metadata[keys[cursor_line]]))
+                value = metadata[keys[cursor_line]]
+                if isinstance(value, str):
+                    cursor_position = min(cursor_position, len(keys[cursor_line]) + 2 + len(value))
+                else:
+                    cursor_position = min(cursor_position, len(keys[cursor_line]) + 2 + len(str(value)))
             elif ch == curses.KEY_LEFT and cursor_position > len(key) + 2:
                 cursor_position -= 1
             elif ch == curses.KEY_RIGHT and cursor_position < max_cursor_position:
                 cursor_position += 1
             elif ch == 127:  # Backspace key
                 if cursor_position > len(key) + 2:
-                    metadata[key] = value[:cursor_position - len(key) - 3] + value[cursor_position - len(key) - 2:]
+                    if isinstance(value, str):
+                        metadata[key] = value[:cursor_position - len(key) - 3] + value[cursor_position - len(key) - 2:]
+                    else:
+                        metadata[key] = str(value)[:cursor_position - len(key) - 3] + str(value)[cursor_position - len(key) - 2:]
                     cursor_position = max(cursor_position - 1, len(key) + 2)
             elif ch == 330:  # Delete key
                 if cursor_position < max_cursor_position:
-                    metadata[key] = value[:cursor_position - len(key) - 2] + value[cursor_position - len(key) - 1:]
+                    if isinstance(value, str):
+                        metadata[key] = value[:cursor_position - len(key) - 2] + value[cursor_position - len(key) - 1:]
+                    else:
+                        metadata[key] = str(value)[:cursor_position - len(key) - 2] + str(value)[cursor_position - len(key) - 1:]
             elif ch < 256:
-                metadata[key] = value[:cursor_position - len(key) - 2] + chr(ch) + value[cursor_position - len(key) - 2:]
+                if isinstance(value, str):
+                    metadata[key] = value[:cursor_position - len(key) - 2] + chr(ch) + value[cursor_position - len(key) - 2:]
+                else:
+                    metadata[key] = str(value)[:cursor_position - len(key) - 2] + chr(ch) + str(value)[cursor_position - len(key) - 2:]
                 cursor_position += 1
 
     stdscr.clear()
     stdscr.refresh()
 
     if quit_key == ":x":
-        save_pdf_metadata(file_path, metadata)
+        save_file_metadata(file_path, metadata)
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python script.py <path_to_pdf>")
+        print("Usage: python script.py <path_to_file>")
         sys.exit(1)
 
     file_path = sys.argv[1]
